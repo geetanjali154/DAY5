f8f5e6b323ffd738fb82bdfdd0ec1f98a533362e
geetanjali <geetanjaligupta154@gmail.com>
2020-02-12 19:51:57 -0500

    extension

--- DAY5/main.swift
+++ DAY5/main.swift
@@ -7,6 +7,34 @@
 //
 
 import Foundation
+extension Double {
+    var km: Double { return self * 1_000.0 }
+    var m: Double { return self }
+    var cm: Double { return self / 100.0 }
+    var mm: Double { return self / 1_000.0 }
+    var ft: Double { return self / 3.28084 }
+    func currency()->String
+    {
+        let st=String.init(format:"$%.2f",self)
+        return st
+    }
+}
 
-print("Hello, World!")
+var meter=100.0
+print(meter.km.currency()   )
+
+extension String{
+    func repeatNTimes(n:Int)
+    {
+        for _ in 1...n
+        {
+            print(self)
+        }
+    }
+}
+
+
+var s="geet"
+print(s.repeatNTimes(n: 10))
+
 
